9ff63dae2259264c2fe48df378777be8c7262472
shubham160623 <salgunde92@gmail.com>
2025-02-27 18:45:53 +0530

    Adding get, post and delete requests

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ app.get("/", (req, res) => {
   res.send("Hello From server2!");
 });
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shubham", lastName: "Algunde" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data Saved successfully!");
+});
+app.delete("/user", (req, res) => {
+  res.send("Data Deleted successfully!");
+});
+
 app.listen(3000, () => {
   console.log("server running on port 3000!");
 });
